f0a8211ab906a5f38ddb39e64299ba104c557d98
danwillm <39023874+danwillm@users.noreply.github.com>
2022-08-09 21:39:55 +0100

    Update knuckle_device_driver.cpp

--- driver/src/device/drivers/knuckle_device_driver.cpp
+++ driver/src/device/drivers/knuckle_device_driver.cpp
@@ -6,7 +6,6 @@ KnuckleDeviceDriver::KnuckleDeviceDriver(const std::string& serial_number) {
 
 void KnuckleDeviceDriver::SetupProperties(vr::PropertyContainerHandle_t& props) {
   vr::VRProperties()->SetInt32Property(props, vr::Prop_ControllerRoleHint_Int32, 2000000000);
-  vr::VRProperties()->SetInt32Property(props, )
 }
 
 void KnuckleDeviceDriver::SetupComponents(vr::PropertyContainerHandle_t& props) {}
